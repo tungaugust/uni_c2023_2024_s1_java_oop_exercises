3a4b2d96a29dc1f753d5603726e4917f0df5fb9e
tungaugust <tungpt96august@gmail.com>
2023-08-17 14:10:52 +0700

    LTHDT-LT-Tuan01. Hoan thanh Bai 1, 2 va 3.

---
+++ LTHDT_LT_Tuan02_PhamThanhTung/src/exercise1/App.java
@@ -0,0 +1,21 @@
+/*
+ *  @ (#) App.java      1.0 17/08/2023
+ *
+ *  Copyright (c) 2023 IUH.
+ *  All rights reserved.
+ */
+
+package exercise1;
+
+/**
+ * Chương trình cho phép nhập họ tên, mã số sinh viên, câu khẩu hiệu và điểm trung bình mong muốn
+ * khi học môn học này từ bàn phím, xuất kết quả.
+ * @author: tungpt
+ * @version: 1.0
+ * @since: August 17, 2023
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("SV1");
+    }
+}
