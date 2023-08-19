2d05de9f85c3371ee77e768793ef7be5f79a7533
tungaugust <tungpt96august@gmail.com>
2023-08-19 18:36:36 +0700

    LTHDT-TH. Hoan thanh Chuong 01-02.Bai 04.

---
+++ LTHDT_TH_PhamThanhTung/src/chapter01_02/exercise04/HelloWorld.java
@@ -0,0 +1,20 @@
+/*
+ *  @ (#) HelloWorld.java      1.0 19/08/2023
+ *
+ *  Copyright (c) 2023 IUH.
+ *  All rights reserved.
+ */
+
+package chapter01_02.exercise04;
+
+/**
+ * Chương trình xuất dòng chữ "Hello World"
+ * @author: tungpt
+ * @version: 1.0
+ * @since: August 19, 2023
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
