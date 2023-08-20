28cbccb71bd1898b658667ef8c1bbafa9f2602b6
tungaugust <tungpt96august@gmail.com>
2023-08-20 22:24:28 +0700

    LTHDT-TH. Hoan thanh Chuong 01-02.Bai 20.

--- LTHDT_TH_PhamThanhTung/src/chapter01_02/exercise20/CountDigitsInString.java
+++ LTHDT_TH_PhamThanhTung/src/chapter01_02/exercise20/CountDigitsInString.java
@@ -7,11 +7,28 @@
 
 package chapter01_02.exercise20;
 
+import java.util.Scanner;
+
 /**
- * @description
+ * Hàm để đếm số lượng ký tự là số có trong chuỗi s
  * @author: tungpt
  * @version: 1.0
  * @since: August 20, 2023
  */
 public class CountDigitsInString {
+    static int countDigits(String s) {
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (Character.isDigit(s.charAt(i))) {
+                count++;
+            }
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nhập chuỗi kí tự: ");
+        String s = scanner.nextLine();
+        System.out.println("Số kí tự là chữ số có trong chuỗi đã nhập là " + countDigits(s) + " kí tự.");
+    }
 }
