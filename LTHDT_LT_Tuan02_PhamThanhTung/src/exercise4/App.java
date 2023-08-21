2688dfe811e868e75d2ed3f8fbf32d2a5ebfe3a5
tungaugust <tungpt96august@gmail.com>
2023-08-22 01:23:56 +0700

    LTHDT-LT-Tuan02. Hoan thanh Bai 4. Bo sung.

--- LTHDT_LT_Tuan02_PhamThanhTung/src/exercise4/App.java
+++ LTHDT_LT_Tuan02_PhamThanhTung/src/exercise4/App.java
@@ -24,17 +24,38 @@ public class App {
          *      + số điện năng hiện tại (số mới) newQuantity
          * Output: Tiền điện phải trả amount
          * Bước 1: Tính lượng điện đã tiêu thụ consumedQuantity = newQuantity - oldQuantity
-         * Bước 2: Xét consumedQuantity để chọn đơn giá unitPrice theo từng mức tương ứng:
-         *      + Nếu 0 <= consumedQuantity <= 50 thì unitPrice = 1.678
-         *      + Nếu 51 <= consumedQuantity <= 100 thì unitPrice = 1.734
-         *      + Nếu 101 <= consumedQuantity <= 200 thì unitPrice = 2.014
-         *      + Nếu 201 <= consumedQuantity <= 300 thì unitPrice = 2.536
-         *      + Nếu 301 <= consumedQuantity <= 400 thì unitPrice = 2.834
-         *      + Nếu consumedQuantity >= 401 thì unitPrice = 2.927
-         *      + Nếu consumedQuantity < 0 thì unitPrice = -1
-         * Bước 3: Xét unitPrice:
-         *      + Nếu unitPrice < 0 thì in ra thông báo "Không thể tính tiền điện!".
-         *      + Nếu unitPrice >= 0 thì tính tiền điện theo công thức: amount = consumedQuantity * unitPrice
+         * Bước 2: Khi consumedQuantity < 0 thì chương trình dừng
+         * Bước 3: Khi consumedQuantity >= 0, gán amount := 0.0, gán biến số lượng điện theo bậc levelQuantity := 0
+         * Bước 4: Gán i := 0
+         * Bước 5: Xét điều kiện i < 6:
+         *      + Nếu điều kiện đúng thì đến Bước 6
+         *      + Nếu điều kiện sai thì đến Bước 8
+         * Bước 6: Xét từng chì số i tương ứng với từng bậc lượng điện tiêu thụ
+         *      + Khi i = 0 (bậc 1):
+         *          Nếu consumedQuantity > 50 thì gán levelQuantity := 50 và giảm consumedQuantity đi 50,
+         *              ngược lại thì gán levelQuantity := consumedQuantity và consumedQuantity := 0.
+         *          amount := amount + levelQuantity * 1.678
+         *      + Khi i = 1 (bậc 2):
+         *          Nếu consumedQuantity > 50 thì gán levelQuantity := 50 và giảm consumedQuantity đi 50,
+         *              ngược lại thì gán levelQuantity := consumedQuantity và consumedQuantity := 0.
+         *          amount := amount + levelQuantity * 1.734
+         *      + Khi i = 2 (bậc 3):
+         *          Nếu consumedQuantity > 100 thì gán levelQuantity := 50 và giảm consumedQuantity đi 100,
+         *              ngược lại thì gán levelQuantity := consumedQuantity và consumedQuantity := 0.
+         *          amount := amount + levelQuantity * 2.014
+         *      + Khi i = 3 (bậc 4):
+         *          Nếu consumedQuantity > 100 thì gán levelQuantity := 50 và giảm consumedQuantity đi 100,
+         *              ngược lại thì gán levelQuantity := consumedQuantity và consumedQuantity := 0.
+         *          amount := amount + levelQuantity * 2.536
+         *      + Khi i = 4 (bậc 5):
+         *          Nếu consumedQuantity > 100 thì gán levelQuantity := 50 và giảm consumedQuantity đi 100,
+         *              ngược lại thì gán levelQuantity := consumedQuantity và consumedQuantity := 0.
+         *          amount := amount + levelQuantity * 2.834
+         *      + Khi i = 5 (bậc 6):
+         *          levelQuantity := consumedQuantity và consumedQuantity := 0.
+         *          amount := amount + levelQuantity * 2.927
+         * Bước 7: Gán i := i + 1, sau đó quay lại Bước 5
+         * Bước 8: In ra kết quả amount
          * */
 
         /*
@@ -52,29 +73,77 @@ public class App {
 
         // bước 1: Tính lượng điện đã tiêu thụ
         int consumedQuantity = newQuantity - oldQuantity;
+        int originConsumedQuantity = consumedQuantity;
+        if (consumedQuantity < 0) {
+            System.out.println("Không thể tính lượng điện đã tiêu thụ.");
+            System.exit(0);
+        }
         // bước 2: Xét consumedQuantity để chọn đơn giá unitPrice theo từng mức tương ứng bằng câu lệnh if
-        double unitPrice;
-        if ((consumedQuantity >= 0) && (consumedQuantity <= 50)) {
-            unitPrice = 1.678;
-        } else if ((consumedQuantity >= 51) && (consumedQuantity <= 100)) {
-            unitPrice = 1.734;
-        } else if ((consumedQuantity >= 101) && (consumedQuantity <= 200)) {
-            unitPrice = 2.014;
-        } else if ((consumedQuantity >= 201) && (consumedQuantity <= 300)) {
-            unitPrice = 2.536;
-        } else if ((consumedQuantity >= 301) && (consumedQuantity <= 400)) {
-            unitPrice = 2.834;
-        } else if (consumedQuantity >= 401) {
-            unitPrice = 2.927;
-        } else {
-            unitPrice = -1;
+        int levelQuantity = 0;
+        double amount = 0.0;
+
+        for (int i = 0; i < 6; i++) {
+            switch (i) {
+                case 0: // 0-50: tiền đối đa = 83.9
+                    if (consumedQuantity > 50) {
+                        levelQuantity = 50;
+                        consumedQuantity -= 50;
+                    } else {
+                        levelQuantity = consumedQuantity;
+                        consumedQuantity = 0;
+                    }
+                    amount += (double) levelQuantity * 1.678;
+                    break;
+                case 1: // 51-100: tiền đối đa = 86.7
+                    if (consumedQuantity > 50) {
+                        levelQuantity = 50;
+                        consumedQuantity -= 50;
+                    } else {
+                        levelQuantity = consumedQuantity;
+                        consumedQuantity = 0;
+                    }
+                    amount += (double) levelQuantity * 1.734;
+                    break;
+                case 2: // 101-200: tiền đối đa = 201.4
+                    if (consumedQuantity > 100) {
+                        levelQuantity = 100;
+                        consumedQuantity -= 100;
+                    } else {
+                        levelQuantity = consumedQuantity;
+                        consumedQuantity = 0;
+                    }
+                    amount += (double) levelQuantity * 2.014;
+                    break;
+                case 3: // 201-300: tiền đối đa = 253.6
+                    if (consumedQuantity > 100) {
+                        levelQuantity = 100;
+                        consumedQuantity -= 100;
+                    } else {
+                        levelQuantity = consumedQuantity;
+                        consumedQuantity = 0;
+                    }
+                    amount += (double) levelQuantity * 2.536;
+                    break;
+                case 4: // 301-400: tiền đối đa = 283.4
+                    if (consumedQuantity > 100) {
+                        levelQuantity = 100;
+                        consumedQuantity -= 100;
+                    } else {
+                        levelQuantity = consumedQuantity;
+                        consumedQuantity = 0;
+                    }
+                    amount += (double) levelQuantity * 2.834;
+                    break;
+                case 5: // 401+
+                    levelQuantity = consumedQuantity;
+                    consumedQuantity = 0;
+                    amount += (double) levelQuantity * 2.927;
+                    break;
+                default: break;
+            }
         }
-        // bước 3: Xét unitPrice
-        if (unitPrice < 0) {
-            System.out.println("Không thể tính tiền điện!");
-        } else {
-            double amount = consumedQuantity * unitPrice;
-            System.out.printf("Tiền điện phải trả cho %d số điện là %.3f VND.\n", consumedQuantity, amount);
-        }
+        // bước 3
+        System.out.printf("Tiền điện phải trả cho %d số điện là %.3f nghìn VND.\n", originConsumedQuantity, amount);
     }
+
 }
---
+++ LTHDT_LT_Tuan03_PhamThanhTung/src/exercise2/main/ElectricityConsumptionCalculator.java
@@ -0,0 +1,16 @@
+/*
+ *  @ (#) ElectricityConsumptionCalculator.java      1.0 21/08/2023
+ * 
+ *  Copyright (c) 2023 IUH.
+ *  All rights reserved.
+ */
+
+package exercise2.main;
+/**
+ *  @description
+ *  @author:    tungpt
+ *  @version:   1.0
+ *  @since:     August 21, 2023
+ */
+public class ElectricityConsumptionCalculator {
+}
