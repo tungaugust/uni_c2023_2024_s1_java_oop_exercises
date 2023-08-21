311612e73383ca0c27ea0f4c5af61780666bd0c1
tungaugust <tungpt96august@gmail.com>
2023-08-21 21:32:38 +0700

    LTHDT-TH. Hoan thanh Chuong 01-02.Bai 29.

--- LTHDT_TH_PhamThanhTung/src/chapter01_02/exercise29/PrintShapesVersion2.java
+++ LTHDT_TH_PhamThanhTung/src/chapter01_02/exercise29/PrintShapesVersion2.java
@@ -28,6 +28,113 @@ public class PrintShapesVersion2 {
             parity = 2; // chẵn
         }
 
-        System.out.println("Hình a. Hình tam giác vuông có chiều dài cạnh góc vuông là " + side + ".");
+        System.out.println("Hình a. Hình tam giác vuông cân có độ dài cạnh góc vuông là " + side + ".");
+        /* ---
+        *
+        * *
+        * * *
+        * * * *
+        * * * * *
+        * * * * * *
+        * * * * * * *
+        --- */
+
+        for (int i = 0; i < side; i++) {
+            for (int j = 0; j < i + 1; j++) {
+                System.out.print(EXISTENCE);
+            }
+            System.out.println();
+        }
+
+        System.out.println("\nHình b. Hình tam giác vuông cân (ngược) có độ dài cạnh góc vuông là " + side + ".");
+        /* ---
+        * * * * * * *
+        * * * * * *
+        * * * * *
+        * * * *
+        * * *
+        * *
+        *
+        --- */
+
+        for (int i = 0; i < side; i++) {
+            for (int j = 0; j < side - i; j++) {
+                System.out.print(EXISTENCE);
+            }
+            System.out.println();
+        }
+
+        System.out.println("\nHình c. Hình tam giác cân có độ dài đường cao là " + side + ".");
+        /* ---
+                          *
+                        * * *
+                      * * * * *
+                    * * * * * * *
+                  * * * * * * * * *
+                * * * * * * * * * * *
+              * * * * * * * * * * * * *
+            * * * * * * * * * * * * * * *
+          * * * * * * * * * * * * * * * * *
+        * * * * * * * * * * * * * * * * * * *
+        --- */
+
+        // Độ dài cạnh đáy bằng = Đường cao * 2 - 1
+        int base = side * 2 - 1;
+        int edgeWidth = 1;  // độ rộng của tầng chóp của tam giác
+        int voidWidth = (base - edgeWidth) / 2; // độ rộng phần trống mỗi bên
+        for (int i = 0; i < side; i++) {
+            for (int j = 0; j < voidWidth; j++) {
+                System.out.print(NONEXISTENCE);
+            }
+            for (int j = 0; j < edgeWidth; j++) {
+                System.out.print(EXISTENCE);
+            }
+            for (int j = 0; j < voidWidth; j++) {
+                System.out.print(NONEXISTENCE);
+            }
+            System.out.println();
+            voidWidth--;
+            edgeWidth += 2;
+        }
+
+        System.out.println("\nHình d. Hình tam giác cân có độ dài đường cao là " + side + "; hình rỗng.");
+        /* ---
+                          *
+                        *   *
+                      *       *
+                    *           *
+                  *               *
+                *                   *
+              *                       *
+            *                           *
+          *                               *
+        * * * * * * * * * * * * * * * * * * *
+        --- */
+
+        edgeWidth = 1;
+        voidWidth = (base - edgeWidth) / 2;
+        for (int i = 0; i < side; i++) {
+            for (int j = 0; j < voidWidth; j++) {
+                System.out.print(NONEXISTENCE);
+            }
+            if ((i == 0) || (i == side - 1)) {
+                for (int j = 0; j < edgeWidth; j++) {
+                    System.out.print(EXISTENCE);
+                }
+            } else {
+                System.out.print(EXISTENCE);
+                for (int j = 0; j < edgeWidth - 2; j++) {
+                    System.out.print(NONEXISTENCE);
+                }
+                System.out.print(EXISTENCE);
+            }
+
+            for (int j = 0; j < voidWidth; j++) {
+                System.out.print(NONEXISTENCE);
+            }
+            System.out.println();
+            voidWidth--;
+            edgeWidth += 2;
+        }
     }
 }
