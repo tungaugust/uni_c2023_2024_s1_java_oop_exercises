1b90e16a1d7d2a98d1b0ac4e06062a54e8cc7915
tungaugust <tungpt96august@gmail.com>
2023-08-21 12:14:23 +0700

    LTHDT-TH. Hoan thanh Chuong 01-02.Bai 24.

---
+++ LTHDT_TH_PhamThanhTung/src/chapter01_02/exercise24/SumFirstIntegers.java
@@ -0,0 +1,29 @@
+/*
+ *  @ (#) SumFirstIntegers.java      1.0 21/08/2023
+ *
+ *  Copyright (c) 2023 IUH.
+ *  All rights reserved.
+ */
+
+package chapter01_02.exercise24;
+
+import java.util.Scanner;
+
+/**
+ * Chương trình tính tổng N số nguyên (dương) đầu tiên
+ * @author: tungpt
+ * @version: 1.0
+ * @since: August 21, 2023
+ */
+public class SumFirstIntegers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nhập số số nguyên: ");
+        int n = Integer.valueOf(scanner.nextLine().trim());
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+        System.out.println("Tổng " + n +" số nguyên (dương) đầu tiên: " + sum);
+    }
+}
