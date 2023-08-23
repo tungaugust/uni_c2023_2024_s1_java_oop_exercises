35214d3c65b0b875527e54b249e36de3bccdbca6
tungaugust <tungpt96august@gmail.com>
2023-08-23 22:19:40 +0700

    LTHDT-TH. Hoan thanh Chuong 03.Bai 1.

--- LTHDT_TH_PhamThanhTung/src/chapter03/exercise01/App.java
+++ LTHDT_TH_PhamThanhTung/src/chapter03/exercise01/App.java
@@ -1,16 +1,30 @@
 /*
  *  @ (#) App.java      1.0 23/08/2023
- * 
+ *
  *  Copyright (c) 2023 IUH.
  *  All rights reserved.
  */
 
 package chapter03.exercise01;
+
+import java.util.Scanner;
+
 /**
- *  @description
- *  @author:    tungpt
- *  @version:   1.0
- *  @since:     August 23, 2023
+ * @description
+ * @author: tungpt
+ * @version: 1.0
+ * @since: August 23, 2023
  */
 public class App {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nhập chiều dài: ");
+        double chieuDai = Double.valueOf(scanner.nextLine().trim());
+        System.out.print("Nhập chiều rộng: ");
+        double chieuRong = Double.valueOf(scanner.nextLine().trim());
+        HinhChuNhat hinhChuNhat = new HinhChuNhat();
+        hinhChuNhat.setChieuDai(chieuDai);
+        hinhChuNhat.setChieuRong(chieuRong);
+        System.out.println(hinhChuNhat);
+    }
 }
--- LTHDT_TH_PhamThanhTung/src/chapter03/exercise01/HinhChuNhat.java
+++ LTHDT_TH_PhamThanhTung/src/chapter03/exercise01/HinhChuNhat.java
@@ -1,16 +1,62 @@
 /*
  *  @ (#) HinhChuNhat.java      1.0 23/08/2023
- * 
+ *
  *  Copyright (c) 2023 IUH.
  *  All rights reserved.
  */
 
 package chapter03.exercise01;
+
 /**
- *  @description
- *  @author:    tungpt
- *  @version:   1.0
- *  @since:     August 23, 2023
+ * @description
+ * @author: tungpt
+ * @version: 1.0
+ * @since: August 23, 2023
  */
 public class HinhChuNhat {
+    private double chieuDai;
+    private double chieuRong;
+
+    public double getChieuDai() {
+        return chieuDai;
+    }
+
+    public void setChieuDai(double chieuDai) {
+        if (chieuDai < 0) {
+            chieuDai = 0;
+        }
+        this.chieuDai = chieuDai;
+    }
+
+    public double getChieuRong() {
+        return chieuRong;
+    }
+
+    public void setChieuRong(double chieuRong) {
+        if (chieuRong < 0){
+            chieuRong = 0;
+        }
+        this.chieuRong = chieuRong;
+    }
+
+    public double dienTich(){
+        return getChieuDai() * getChieuRong();
+    }
+
+    public double chuVi(){
+        if ((getChieuRong() == 0) || (getChieuDai() == 0)) {
+            return 0;
+        }
+        return 2 * (getChieuDai() + getChieuRong());
+    }
+
+    @Override
+    public String toString() {
+        return "HinhChuNhat[" +
+                "chiều dài =" + chieuDai +
+                ", chiều rộng =" + chieuRong +
+                ", diện tích =" + dienTich() +
+                ", chu vi =" + chuVi() +
+                ']';
+    }
 }
