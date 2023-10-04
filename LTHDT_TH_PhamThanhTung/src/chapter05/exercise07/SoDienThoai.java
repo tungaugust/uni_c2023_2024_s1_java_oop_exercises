2cbdfc3694dd2dd7f3502300cd9531f1bf6f4668
tungaugust <tungpt96august@gmail.com>
2023-10-04 16:31:05 +0700

    LTHDT-TH. Hoan thanh Chuong 05.Bai 07 (Dang thuc hien 1).

---
+++ LTHDT_TH_PhamThanhTung/src/chapter05/exercise07/SoDienThoai.java
@@ -0,0 +1,16 @@
+/*
+ *  @ (#) SoDienThoai.java      1.0 04/10/2023
+ *  
+ *  Copyright (c) 2023 IUH.
+ *  All rights reserved.
+ */
+
+package chapter05.exercise07;
+/**
+ *  @description
+ *  @author:    tungpt
+ *  @version:   1.0
+ *  @since:     October 04, 2023
+ */
+public class SoDienThoai {
+}
